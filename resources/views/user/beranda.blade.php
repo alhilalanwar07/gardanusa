64d5da9157a74076d1abb1549a9fb70ec4be89d3
Alhilal Anwar <alhilalanwar07@gmail.com>
2025-03-23 05:12:37 +0800

    add beranda

---
+++ resources/views/user/beranda.blade.php
@@ -0,0 +1,18 @@
+<x-frontend>
+    @section('title', 'Beranda')
+
+    <livewire:beranda.hero />
+    <liveire:beranda.about />
+    <liveire:beranda.features />
+    <liveire:beranda.features-cards />
+    <livewire:beranda.features2 />
+    <livewire:beranda.call-to-action />
+    <livewire:beranda.clients />
+    <livewire:beranda.testimonials />
+    <livewire:beranda.stats />
+    <livewire:beranda.services />
+    <livewire:beranda.pricing />
+    <livewire:beranda.faq />
+    <livewire:beranda.call-to-actions-2 />
+    <livewire:beranda.contact />
+</x-frontend>
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\{Route, Auth};
 // disable register, reset password
 Auth::routes(['register' => false, 'reset' => false]);
 
-// jika ke /, redirect ke /login
-Route::redirect('/', '/login');
-
 Route::middleware('auth')->group(function () {
     Route::view('dashboard', 'dashboard')->name('home');
     Route::view('manajemen-user', 'manajemen-user')->name('admin.manajemen-user');
     Route::view('profil', 'profil')->name('profil');
 });
+
+// beranda
+Route::view('/', 'user.beranda')->name('beranda');
